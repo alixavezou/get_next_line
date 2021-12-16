bf9ec7bb02c377f877dfa9398799119dfbc52498
alixavezou <73618982+alixavezou@users.noreply.github.com>
2021-12-16 15:39:22 +0100

    Create get_next_line.c

---
+++ get_next_line.c
@@ -0,0 +1,98 @@
+#include "get_next_line.h"
+
+int	ft_checkendofline(char *buffer_save)
+{
+	int	i;
+
+	i = 0;
+	while (buffer_save[i])
+	{
+		if (buffer_save[i] == '\n')
+			return (0);
+		i++;
+	}	
+	return (1);
+}
+
+char	*ft_print_line(char *buffer_save)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!buffer_save)
+		return (NULL);
+	while (buffer_save[i] && buffer_save[i] != '\n')
+		i++;
+	line = ft_substr(buffer_save, 0, i + 1);
+	return (line);
+}
+
+char	*ft_get_buffer_save(char *buffer_save)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = ft_strlen(buffer_save);
+	if (!buffer_save)
+		return (NULL);
+	while (buffer_save[i])
+	{
+		if (buffer_save[i] == '\n' && buffer_save[i + 1] != '\0')
+		{
+			i += 1;
+			buffer_save = ft_substr(buffer_save, i, len - i + 1);
+			break ;
+		}
+		else if (buffer_save[i + 1] == '\0')
+			buffer_save[0] = '\0';
+		i++;
+	}
+	return (buffer_save);
+}
+
+char	*ft_read(int fd, char *buffer_read)
+{
+	int			ret;
+	char		*line;
+	static char	*buffer_save = NULL;
+
+	ret = 1;
+	while (ret > 0)
+	{
+		ret = read(fd, buffer_read, BUFFER_SIZE);
+		if (ret <= 0)
+			break ;
+		buffer_read[ret] = '\0';
+		if (buffer_save == NULL)
+			buffer_save = ft_strdup(buffer_read);
+		else
+			buffer_save = ft_strjoin(buffer_save, buffer_read);
+		if (ft_checkendofline(buffer_save) == 0)
+			break ;
+	}
+	line = ft_print_line(buffer_save);
+	buffer_save = ft_get_buffer_save(buffer_save);
+	if (line == 0)
+		free(line);
+	if (ret == 0 && line == 0 && buffer_save == 0)
+		free(buffer_save);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	char	*line;
+	char	buffer_read[BUFFER_SIZE + 1];
+
+	if (fd > 1024 || fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = ft_read(fd, buffer_read);
+	if (line == 0)
+	{
+		free(line);
+		line = NULL;
+	}
+	return (line);
+}
