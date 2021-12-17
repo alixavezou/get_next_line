ae0277c7071348b18a7b395f595fe75a87822fa2
alixavezou <73618982+alixavezou@users.noreply.github.com>
2021-12-17 17:25:56 +0100

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+int		ft_checkendofline(char *buffer_save);
+size_t	ft_strlen(char *str);
+
+char	*ft_print_line(char *buffer_save);
+char	*ft_get_buffer_save(char *buffer_save);
+char	*ft_read(int fd, char *buffer_read);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strdup(char *src);
+
+#endif
