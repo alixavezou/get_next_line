13fbb837a676652201452a1f659dd7ad064b5876
alixavezou <73618982+alixavezou@users.noreply.github.com>
2021-12-27 01:23:08 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,16 +1,50 @@
 #include "get_next_line.h"
 
+size_t	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	if (!src)
+		return (NULL);
+	j = ft_strlen(src);
+	dest = malloc(sizeof(char) * (j + 1));
+	if (!dest)
+		return (NULL);
+	while (i < j)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 char	*ft_strjoin(char *s1, char *s2)
 {
-	char			*dest;
-	size_t			i;
-	size_t			j;
+	size_t	i;
+	size_t	j;
+	char	*dest;
 
 	i = 0;
 	j = 0;
 	if (!s1)
 		return (NULL);
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (!dest)
 		return (NULL);
 	while (s1[i])
@@ -26,27 +60,27 @@ char	*ft_strjoin(char *s1, char *s2)
 	}
 	dest[i] = '\0';
 	free(s1);
-	return ((char *)dest);
+	return (dest);
 }
 
 char	*ft_substr(char *s, unsigned int start, size_t len)
 {
 	char			*dest;
 	unsigned int	i;
-	unsigned int	j;
+	unsigned int	len_bis;
 
 	i = 0;
 	if (!s)
 		return (NULL);
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
-	j = ft_strlen(s + start);
-	if (j < len)
-		len = j;
+	len_bis = ft_strlen(s + start);
+	if (len > len_bis)
+		len = len_bis;
 	dest = malloc(sizeof(char) * len + 1);
 	if (!dest)
 		return (NULL);
-	while (s[start] && i < len)
+	while (i < len)
 	{
 		dest[i] = s[start];
 		i++;
@@ -54,36 +88,4 @@ char	*ft_substr(char *s, unsigned int start, size_t len)
 	}
 	dest[i] = '\0';
 	return (dest);
-}
-
-char	*ft_strdup(char *src)
-{
-	int		i;
-	int		j;
-	char	*dest;
-
-	i = 0;
-	j = ft_strlen(src);
-	dest = malloc(sizeof(char) * (j + 1));
-	if (!dest)
-		return (NULL);
-	while (i < j)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-size_t	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i])
-		i++;
-	return (i);
 }
